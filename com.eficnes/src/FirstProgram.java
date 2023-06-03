fe43ce5fa949e8c1e106a678b2900e3646e71457
Sheshidhar1 <Sheshidhars1@gmail.com>
2023-06-03 11:46:38 -0500

    Add new code

---
+++ com.eficnes/src/FirstProgram.java
@@ -0,0 +1,10 @@
+public class FirstProgram {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 20;
+
+        int sum = a + b ;
+
+        System.out.printf("Sum of two numbers:"  +sum);
+    }
+}
